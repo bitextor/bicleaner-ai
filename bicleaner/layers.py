8e22bd748889ef05f6c63497221a9f428c1e8b46
ZJaume <jzaragoza@prompsit.com>
2021-03-18 11:51:40 +0000

    Revert "Remove Huggingface Tansformers in master"
    This reverts commit b7c56c0caf8ce35bf4216271bc86213344f79c4a.

--- bicleaner/layers.py
+++ bicleaner/layers.py
@@ -1,3 +1,4 @@
+from transformers.modeling_tf_utils import get_initializer
 from tensorflow.keras import layers
 from tensorflow import keras
 import tensorflow as tf
@@ -54,3 +55,27 @@ class TransformerBlock(layers.Layer):
         ffn_output = self.ffn(out1)
         ffn_output = self.dropout2(ffn_output, training=training)
         return self.layernorm2(out1 + ffn_output)
+
+class BCClassificationHead(layers.Layer):
+    """Head for sentence-level classification tasks."""
+
+    def __init__(self, config, **kwargs):
+        super().__init__(**kwargs)
+        self.dense = layers.Dense(
+            config.hidden_size,
+            kernel_initializer=get_initializer(config.initializer_range),
+            activation=config.hidden_activation,
+            name="dense",
+        )
+        self.dropout = layers.Dropout(config.hidden_dropout_prob)
+        self.out_proj = layers.Dense(
+            config.num_labels, kernel_initializer=get_initializer(config.initializer_range), name="out_proj"
+        )
+
+    def call(self, features, training=False):
+        x = features[:, 0, :]  # take <s> token (equiv. to [CLS])
+        x = self.dropout(x, training=training)
+        x = self.dense(x)
+        x = self.dropout(x, training=training)
+        x = self.out_proj(x)
+        return x
