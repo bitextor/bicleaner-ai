ff5355b7bae1fc8e765e35c0c5cd959461ef32b6
ZJaume <jzaragoza@prompsit.com>
2021-06-15 07:57:49 +0000

    Update langpacks downloading script

--- utils/download-pack.sh
+++ utils/download-pack.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 usage() {
-    echo "Script to download Bicleaner language packs."
-    echo "It will try to download lang1-lang2.tar.gz and if it does not exist it will try lang2-lang1.tar.gz ."
+    echo "Script to download Bicleaner AI language packs."
+    echo "It will try to download {lite,full}-lang1-lang2.tgz and if it does not exist it will try {lite,full}-lang2-lang1.tgz ."
     echo
     echo "Usage: `basename $0` <lang1> <lang2> <download_path>"
     echo "      <lang1>         Language 1."
     echo "      <lang2>         Language 2."
+    echo "      {lite,full}     Download lite or full model."
     echo "      <download_path> Path where downloaded language pack should be placed."
 }
 
@@ -14,37 +15,44 @@ invalid_url(){
     wget -S --spider -o - $1 | grep -q '404 Not Found'
 }
 
-if [[ $# -lt 2 ]]
+if [[ $# -lt 3 ]]
 then
-    echo "Wrong number of arguments: $@" 2>&1
-    usage 2>&1
+    echo "Wrong number of arguments: $@" >&2
+    usage >&2
     exit 1
 fi
 
-URL="https://github.com/bitextor/bicleaner-data/releases/latest/download"
+URL="https://github.com/bitextor/bicleaner-ai-data/releases/latest/download"
 L1=$1
 L2=$2
-if [ "$3" != "" ]; then
-    DOWNLOAD_PATH=$3
+if [ "$3" != "lite" ] && [ "$3" != "full" ]; then
+    echo "Model type must be 'lite' or 'full' not '$3'" 1>&2
+    usage >&2
+    exit 1
+fi
+TYPE=$3
+if [ "$4" != "" ]; then
+    DOWNLOAD_PATH=$4
 else
     DOWNLOAD_PATH="."
 fi
 
-if invalid_url $URL/$L1-$L2.tar.gz
+
+if invalid_url $URL/$TYPE-$L1-$L2.tgz
 then
     >&2 echo $L1-$L2 language pack does not exist, trying $L2-$L1...
-    if invalid_url $URL/$L2-$L1.tar.gz
+    if invalid_url $URL/$TYPE-$L2-$L1.tgz
     then
         >&2 echo $L2-$L1 language pack does not exist
     else
-        wget -P $DOWNLOAD_PATH $URL/$L2-$L1.tar.gz
-        tar xvf $DOWNLOAD_PATH/$L2-$L1.tar.gz -C $DOWNLOAD_PATH
-        rm $DOWNLOAD_PATH/$L2-$L1.tar.gz
+        wget -P $DOWNLOAD_PATH $URL/$TYPE-$L2-$L1.tgz
+        tar xvf $DOWNLOAD_PATH/$TYPE-$L2-$L1.tgz -C $DOWNLOAD_PATH
+        rm $DOWNLOAD_PATH/$TYPE-$L2-$L1.tgz
     fi
 else
-    wget -P $DOWNLOAD_PATH $URL/$L1-$L2.tar.gz
-    tar xvf $DOWNLOAD_PATH/$L1-$L2.tar.gz -C $DOWNLOAD_PATH
-    rm $DOWNLOAD_PATH/$L1-$L2.tar.gz
+    wget -P $DOWNLOAD_PATH $URL/$TYPE-$L1-$L2.tgz
+    tar xvf $DOWNLOAD_PATH/$TYPE-$L1-$L2.tgz -C $DOWNLOAD_PATH
+    rm $DOWNLOAD_PATH/$TYPE-$L1-$L2.tgz
 fi
 
 echo Finished
