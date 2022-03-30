88e32ffbbf49a98f2e8f3e82f7889432a099a19b
Cristian García Romero <cgr71ii@gmail.com>
2022-03-30 13:35:36 +0200

    Update conda build scripts

--- conda_build/bicleaner-ai/build.sh
+++ conda_build/bicleaner-ai/build.sh
@@ -3,6 +3,4 @@ export PIP_NO_INDEX="False" # We are downloading requisites from PyPi
 export PIP_NO_DEPENDENCIES="False" # We need the dependencies from our defined dependencies
 export PIP_IGNORE_INSTALLED="False" # We need to take into account the dependencies
 
-$PYTHON -m pip install setuptools_rust
-
 $PYTHON -m pip install .
